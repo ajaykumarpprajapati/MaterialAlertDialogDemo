19fb398880ccc1340f6cb6c5a3d7867a001736c0
ajay <ajay.prajapati@neoquant.com>
2019-06-07 12:15:16 +0530

    Change Color of dialog action button by two way

--- app/src/main/java/india/ajay/materialalertdialogdemo/MainActivity.kt
+++ app/src/main/java/india/ajay/materialalertdialogdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package india.ajay.materialalertdialogdemo
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupView() {
-        builder = AlertDialog.Builder(this@MainActivity)
+        //Passing style in 2nd paramter will change by default color of dialog action
+        builder = AlertDialog.Builder(this@MainActivity, R.style.AlertDialogTheme)
         builder?.setMessage(getString(R.string.discard_draft))
         builder?.setPositiveButton(getString(R.string.discard)) { _, _ ->
             Log.d(TAG, "Clicked Postive button")
@@ -29,5 +31,17 @@ class MainActivity : AppCompatActivity() {
         }
         alerDialog = builder?.create()
         alerDialog?.show()
+
+        /*First way to change color of button text*/
+        //Changing color of positive button
+        /*alerDialog?.getButton(AlertDialog.BUTTON_POSITIVE)
+            ?.setTextColor(ContextCompat.getColor(this, R.color.colorAlertDialog))
+
+        //Changing color of negative button
+        alerDialog?.getButton(AlertDialog.BUTTON_NEGATIVE)
+            ?.setTextColor(ContextCompat.getColor(this, R.color.colorAlertDialog))*/
+
+        /*2nd way to change color of button text*/
+
     }
 }
